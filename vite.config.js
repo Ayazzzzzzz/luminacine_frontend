615ec34f73fc04a0c36962cab2a2319b3642d9e8
Capstone_GreenGuard <larasayodya04@gmail.com>
2025-06-03 00:32:40 +0700

    coba ganti vitenya

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path' // tambahkan ini
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
+    host: true,
+    strictPort: true,
     port: 3000,
   },
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.jsx?$/,
-    exclude: [],
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'), // ⬅️ tambahkan baris ini
-    },
-  },
-})
+});
